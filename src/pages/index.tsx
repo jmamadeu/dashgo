540579bb707b06599453f92c5cf40ac803e84d10
João Amadeu <jmamadeu2000@gmail.com>
2021-12-13 19:58:57 +0100

    feat: login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,38 @@
+import { Button, Flex, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Input } from "../components/form/input";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>DashGo</title>
-        <meta
-          name="description"
-          content="Take your dashboards to another level"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Home </h1>
-    </div>
-  );
-};
+const Home: NextPage = () => (
+  <Flex as="main" justify="center" align="center" h="100vh">
+    <Head>
+      <title>DashGo</title>
+      <meta
+        name="description"
+        content="Take your dashboards to another level"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Flex
+      as="form"
+      width="100%"
+      maxW={360}
+      bg="gray.800"
+      p={8}
+      borderRadius={8}
+      flexDir="column"
+    >
+      <Stack spacing={4}>
+        <Input name="email" type="email" label="Your email" />
+        <Input name="password" type="password" label="Your password" />
+      </Stack>
+
+      <Button type="button" marginTop={6} colorScheme="pink">
+        Sign In
+      </Button>
+    </Flex>
+  </Flex>
+);
 
 export default Home;
