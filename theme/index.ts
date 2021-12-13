8cdbd81406a0ae937aad413fabc8cc9beb023a96
João Amadeu <jmamadeu2000@gmail.com>
2021-12-13 18:11:10 +0100

    chore: custom theme configuration

---
+++ theme/index.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react";
+import { globalStyles } from "./styles";
+
+export const COLORS = {
+  highlight: "#FF57B2",
+  heading: "#F8F8F8",
+  background: "#1A1D23",
+  info: "#BBBBBB",
+  body: "#D7D7D7"
+};
+
+export const FONTS = {
+  body: "Inter"
+};
+
+const overrides = {
+  colors: COLORS,
+  fonts: FONTS,
+  ...globalStyles,
+  components: {}
+};
+
+export default extendTheme(overrides);
---
+++ theme/styles.ts
@@ -0,0 +1,7 @@
+export const globalStyles = {
+  styles: {
+    global: {
+      body: {}
+    }
+  }
+};
